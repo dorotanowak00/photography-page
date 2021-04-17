57e43d8fc2b48ac60c5e3fde6aa81dfeaa911672
dorotanowak00 <dorota.nowak00@gmail.com>
2021-04-17 16:17:15 +0200

    added fixes to nav animations

---
+++ JavaScript/animation.js
@@ -0,0 +1,12 @@
+/*function animation() {
+	var anim = document.getElementById("animation");
+	anim.classList.add("reverse");
+	anim.removeEventListener("mouseover", add_anim);
+
+};*/
+
+setTimeout(function() {
+    document.getElementById("animation1").className="";
+    document.getElementById("animation2").className="";
+    document.getElementById("animation3").className="";
+},500);
