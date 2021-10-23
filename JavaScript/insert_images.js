12e39aac57947628bb70e10c376529fd477d5354
dorotanowak00 <dorota.nowak00@gmail.com>
2021-10-23 14:42:59 +0200

    added new file paths

--- JavaScript/insert_images.js
+++ JavaScript/insert_images.js
@@ -3,24 +3,22 @@ var images_stylized = new Array();
 var images_model = new Array();
 
 images_white[0] = new Image();
-images_white[0].src = 'img/white/img1.jpg';
+images_white[0].src = 'assets/home/white/img1.jpg';
 images_white[1] = new Image();
-images_white[1].src = 'img/white/img2.jpg';
+images_white[1].src = 'assets/home/white/img2.jpg';
 images_white[2] = new Image();
-images_white[2].src = 'img/white/img3.jpg';
+images_white[2].src = 'assets/home/white/img3.jpg';
 
 
 images_stylized[0] = new Image();
-images_stylized[0].src = 'img/stylized/img1.jpg';
+images_stylized[0].src = 'assets/home/stylized/img1.jpg';
 images_stylized[1] = new Image();
-images_stylized[1].src = 'img/stylized/img2.jpg';
+images_stylized[1].src = 'assets/home/stylized/img2.jpg';
 
 images_model[0] = new Image();
-images_model[0].src = 'img/model/img1.jpg';
+images_model[0].src = 'assets/home/model/img1.jpg';
 images_model[1] = new Image();
-images_model[1].src = 'img/model/img2.jpg';
-
-
+images_model[1].src = 'assets/home/model/img2.jpg';
 
 function gallery(gallery_type) {
 	let src = eval(`images_${gallery_type}[0].src`);
