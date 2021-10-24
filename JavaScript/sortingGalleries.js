ab0231e0537cd924a302fff264fa3d3b80da1003
dorotanowak00 <dorota.nowak00@gmail.com>
2021-10-24 18:56:33 +0200

    added filtering in gallery

---
+++ JavaScript/sortingGalleries.js
@@ -0,0 +1,36 @@
+
+let divContainer = document.getElementById('container');
+let galleries = [...document.getElementsByClassName('img_container')];
+
+let buttons = [...document.getElementsByTagName('button')];
+
+// let allButton = document.getElementById('all');
+// let packshotButton = document.getElementById('packshot');
+// let modelButton = document.getElementById('model');
+// let stylizedButton = document.getElementById('stylized');
+
+
+
+filter = (e) => {
+    galleries.forEach(item => item.style.display = "block");
+    let button = e.target;
+    let buttonId = e.target.id;
+
+    if (e.target.id === "all") {
+        galleries.forEach(item => item.style.display = "block");
+        return;
+    };
+
+    galleries.forEach(item => {
+        if (!item.className.includes(buttonId)) {
+            item.style.display = "none";
+        } else {
+            return
+        }
+    })
+    
+}
+
+buttons.forEach(item => {
+    item.addEventListener('click', filter)
+});
