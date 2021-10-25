794e42ea828affe6522ae8a00c84287418332f48
dorotanowak00 <dorota.nowak00@gmail.com>
2021-10-25 20:51:48 +0200

    submitting a form doesnt cause page refresh

--- PHP/email.php
+++ PHP/email.php
@@ -1,9 +1,9 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
+// if(!isset($_POST['submit']))
+// {
+// 	//This page should not be accessed directly. Need to submit the form.
+// 	echo "error; you need to submit the form!";
+// }
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $title = $_POST['title'];
@@ -36,8 +36,10 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
 mail($to,$email_subject,$email_body,$headers);
+echo "Formularz wysłany. Dziękujemy za kontakt!"; 
+
 //done. redirect to thank-you page.
-header('Location: ../thank-you.html');
+// header('Location: ../thank-you.html');
 
 
 // Function to validate against any email injection attempts
